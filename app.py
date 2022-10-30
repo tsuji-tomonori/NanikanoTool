2eeb876a9e7e64f382b981da862115748d46e80d
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-10-30 23:33:42 +0900

    構成変更

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_sqs as sqs,
     aws_sns as sns,
+    aws_ssm as ssm,
     aws_lambda_event_sources as lambda_event,
     Duration,
     Tags,
@@ -48,12 +49,32 @@ class SugokuWaruiTool(Stack):
             layers=[layer]
         )
 
+        get_url_fn = lambda_.Function(
+            self, "get_url_fn",
+            code=lambda_.Code.from_asset("src/get_url"),
+            handler="lambda_function.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            timeout=Duration.seconds(60),
+            environment=self.node.try_get_context("lambda_env"),
+            memory_size=256,
+            layers=[layer]
+        )
+
         saveimg_trigger = sqs.Queue(self, "saveimg_trigger")
         saveimg_fn.add_event_source(
             lambda_event.SqsEventSource(saveimg_trigger)
         )
         saveimg_trigger.grant_send_messages(saveimg_fn.role)
-        saveimg_fn.add_environment("QUEUE_URL", saveimg_trigger.queue_url)
+        saveimg_trigger.grant_send_messages(get_url_fn.role)
+        saveimg_fn.add_environment("IMG_QUEUE_URL", saveimg_trigger.queue_url)
+        get_url_fn.add_environment("IMG_QUEUE_URL", saveimg_trigger.queue_url)
+
+        get_url_trigger = sqs.Queue(self, "get_url_trigger")
+        get_url_fn.add_event_source(
+            lambda_event.SqsEventSource(get_url_trigger)
+        )
+        get_url_trigger.grant_send_messages(get_url_fn.role)
+        get_url_fn.add_environment("URL_QUEUE_URL", get_url_trigger.queue_url)
 
         bucket = s3.Bucket(self, "saveimg_bucket")
         bucket.grant_put(saveimg_fn.role)
@@ -70,11 +91,22 @@ class SugokuWaruiTool(Stack):
             write_capacity=1,
         )
         db.grant_read_data(saveimg_fn.role)
+        db.grant_write_data(get_url_fn.role)
         saveimg_fn.add_environment("DB_NAME", db.table_name)
+        get_url_fn.add_environment("DB_NAME", db.table_name)
 
         fin_topic = sns.Topic(self, "fin_topic")
         fin_topic.grant_publish(saveimg_fn.role)
         saveimg_fn.add_environment("TOPICK_ARN", fin_topic.topic_arn)
+        get_url_fn.add_environment("TOPICK_ARN", fin_topic.topic_arn)
+
+        param = ssm.StringParameter.from_secure_string_parameter_attributes(
+            self, self.node.try_get_context("URL_PARAM"),
+            version=1,
+            parameter_name=self.node.try_get_context("URL_PARAM"),
+        )
+        param.grant_read(get_url_fn.role)
+        get_url_fn.add_environment("URL_PARAM", param.parameter_name)
 
 
 app = cdk.App()
